a6e4ee7637efad04c76e46631c900f870466f3eb
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-01-16 21:40:32 +0530

    Add odd_even.c file
    A program to check if a number is even or odd using if-else statements.

---
+++ odd_even.c
@@ -0,0 +1,13 @@
+//Write a program to find even and odd no by using if else statement.
+
+#include<stdio.h>
+void main()
+{
+int x;
+printf("enter a number");
+scanf("%d",&x);
+if(x%2==0)
+printf("even number");
+else
+printf("odd number");
+}
