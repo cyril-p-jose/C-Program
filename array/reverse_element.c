4f74f8770c3502e7c670736b78890028b87b62d9
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-02-02 22:13:17 +0530

    Update reverse_element.c

--- array/reverse_element.c
+++ array/reverse_element.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int a[5], i;
+    int n, i;
 
-    printf("Enter 5 elements:\n");
-    for (i = 0; i < 5; i++) {
-        scanf("%d", &a[i]);
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-
     printf("Reversed array:\n");
-    for (i = 4; i >= 0; i--) {
-        printf("%d ", a[i]);
+    // Reversing the array
+    for(i=n-1; i >=0; --i) {
+        printf("%d ",arr[i]);
     }
 
+  
     return 0;
 }
