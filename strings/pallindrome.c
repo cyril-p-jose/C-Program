0b8e3182dcf9408834c97285c1a4bd5f14186cbc
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-02-14 10:23:47 +0530

    Create pallindrome.c

---
+++ strings/pallindrome.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+    int i = 0, length = 0;
+    int isPalindrome = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Find length manually
+    while (str[length] != '\0') 
+    {
+        length++;
+    }
+
+    // Check palindrome
+    for (i = 0; i < length / 2; i++) 
+    {
+        if (str[i] != str[length - i - 1]) 
+        {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+        printf("The string is a Palindrome.\n");
+    else
+        printf("The string is NOT a Palindrome.\n");
+
+    return 0;
+}
